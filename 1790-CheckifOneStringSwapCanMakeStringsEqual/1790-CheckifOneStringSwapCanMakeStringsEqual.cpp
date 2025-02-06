638c73d56d13f649d957976276dc48171b870713
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-02-06 21:16:22 +0530

    Create file

---
+++ 1790-CheckifOneStringSwapCanMakeStringsEqual/1790-CheckifOneStringSwapCanMakeStringsEqual.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int n = s1.length();
+        unordered_map<char, int> mp;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(count > 2)
+                return false;
+            if(s1[i] != s2[i]){
+                mp[s1[i]]++;
+                mp[s2[i]]--;
+                count++;
+            }
+        }
+        if(!(mp.size() == 2 || mp.size() == 0))
+            return false;
+        for(auto it : mp){
+            if(it.second != 0)
+                return false;
+        }
+        return true;
+
+    }
+};
