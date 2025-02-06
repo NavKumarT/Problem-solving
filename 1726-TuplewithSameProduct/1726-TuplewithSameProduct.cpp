4497fd65e093644f7a204132d31893cc41291055
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-02-06 23:03:26 +0530

    Update file 2/6/2025, 11:03:26 PM

--- 1726-TuplewithSameProduct/1726-TuplewithSameProduct.cpp
+++ 1726-TuplewithSameProduct/1726-TuplewithSameProduct.cpp
@@ -7,14 +7,12 @@ public:
             for(int j = i+1; j < n; j++){
                 int product = nums[i] * nums[j];
                 mp[product]++;
-                // mp[product].push_back(j);
             }
         }
         int ans = 0;
         for(auto it : mp){
             if(it.second >= 2){
                 ans += 8 * ((it.second * (it.second - 1))/2);
-                // cout << it.first << " " << it.second.size() << " " << pow(2, (it.second.size()/2)+1) << endl;
             }
         }
         return ans;
