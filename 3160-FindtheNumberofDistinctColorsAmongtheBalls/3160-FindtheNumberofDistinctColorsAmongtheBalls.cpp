a913513b7d3579f77b239c351c22f2b6cb477de5
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-02-07 12:02:22 +0530

    Update file 2/7/2025, 12:02:22 PM

--- 3160-FindtheNumberofDistinctColorsAmongtheBalls/3160-FindtheNumberofDistinctColorsAmongtheBalls.cpp
+++ 3160-FindtheNumberofDistinctColorsAmongtheBalls/3160-FindtheNumberofDistinctColorsAmongtheBalls.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        // map : we store the count of occurences for each color 
-        unordered_map<int, int> mp;
-        // Color vector : To store the color of the balls so that we can reduce the count for prev color 
-        // and increase the color for the new color
-        // vector<int> colors(limit+1, -1);
-        unordered_map<int, int> colors;
-        // To return the ans
+        // we store the count of occurences for each color in the first map 
+        // we then store the color that each ball has in the second map 
+        
+        unordered_map<int, int> mp;  // color : count
+        unordered_map<int, int> colors;   // ball : color 
         vector<int> ans; 
+
         for(int i = 0; i < queries.size(); i++){
-            int temp = colors[queries[i][0]];
-            // if(temp != -1)
-            if(colors.find(queries[i][0]) != colors.end() && mp[temp] > 0)
-                mp[temp]--;
-            if(mp[temp] == 0)
-                mp.erase(temp);
-            colors[queries[i][0]] = queries[i][1];
+            int currentBall = queries[i][0];
+            int currentColorOfBall = colors[queries[i][0]];
+            // decrease the count of current color and remove it from the map if count becomes 0
+            if(colors.find(currentBall) != colors.end() && mp[currentColorOfBall] > 0)
+                mp[currentColorOfBall]--;
+            if(mp[currentColorOfBall] == 0)
+                mp.erase(currentColorOfBall);
+            // update the color of the ball and also update the count of the new color
+            colors[currentBall] = queries[i][1];
             mp[queries[i][1]]++;
             ans.push_back(mp.size());
         }
