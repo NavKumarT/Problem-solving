e7341174a40da2dac974f7b9973e4e0a1e5d7415
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-02-08 10:55:08 +0530

    Create file

---
+++ 2349-DesignaNumberContainerSystem/2349-DesignaNumberContainerSystem.cpp
@@ -0,0 +1,32 @@
+class NumberContainers {
+public:
+    unordered_map<int,int> mp1;
+    unordered_map<int, set<int>> mp2;
+    NumberContainers() {
+        mp1.clear();
+        mp2.clear();
+    }
+    
+    void change(int index, int number) {
+        if(mp1.find(index) != mp1.end()){
+            mp2[mp1[index]].erase(index);
+            if(mp2[mp1[index]].size() == 0)
+                mp2.erase(mp1[index]);
+        }
+        mp1[index] = number;
+        mp2[number].insert(index);
+    }
+    
+    int find(int number) {
+        if(mp2.find(number) != mp2.end())
+            return *mp2[number].begin();
+        return -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
