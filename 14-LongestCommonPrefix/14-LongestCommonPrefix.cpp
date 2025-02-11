e2a6afb1395e737665c1c90e1fb93da2292977b7
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-02-11 22:05:08 +0530

    Create file

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 1)
+            return strs[0];
+        string sub = func(strs[0], strs[1]);
+        cout << sub << endl;
+        for(int i = 1; i < strs.size(); i++){
+            sub = func(sub, strs[i]);
+        }
+        return sub;
+    }
+
+    string func(string s1, string s2){
+        int m = s1.length();
+        int n = s2.length();
+        int i = 0; 
+        int j = 0;
+        string ans = "";
+        while(i < m && j < n){
+            if(s1[i] == s2[j]){
+                ans += s1[i];
+                i++;
+                j++;
+            } else if(s1[i] != s2[j]){
+                break;
+            }
+        }
+        return ans;
+    }
+};
