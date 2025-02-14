3ec1252bbaa4669c68463442fc1ea9f9dc881055
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-02-14 23:08:44 +0530

    Create file

---
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> memo(n+1);
+        memo[0] = 0;
+        memo[1] = 0;
+        for(int i = 2; i <= n; i++){
+            memo[i] = min(memo[i-1] + cost[i-1], memo[i-2] + cost[i-2]);
+        }
+        return memo[n];
+    }
+};
