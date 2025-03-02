62d729437db814dff6cea087072cb7468ec04da4
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-03-02 22:38:22 +0530

    File updated at 3/2/2025, 10:38:23 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -1,3 +1,4 @@
+// Last updated: 3/2/2025, 10:38:23 PM
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
@@ -8,10 +9,6 @@ public:
     }
 
     int dp(int m, int n, string text1, string text2, vector<vector<int>> &memo){
-        // if(i >= m || j >= n)
-        //     return 0;
-        // if(memo[i][j] != -1)
-        //     return memo[i][j];
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 if(text1[i-1] == text2[j-1]){
@@ -22,15 +19,5 @@ public:
             }
         }
         return memo[m][n];
-        // else {
-        //     int res;
-        //     if(text1[i] == text2[j]){
-        //     res =  1 + dp(i+1, j+1, m, n, text1, text2, memo);
-        //     } else {
-        //         res =  max(dp(i+1, j, m, n, text1, text2, memo), dp(i, j+1, m, n, text1, text2, memo));
-        //     }
-        //     memo[i][j] = res;
-        //     return res;
-        // }
     }
 };
