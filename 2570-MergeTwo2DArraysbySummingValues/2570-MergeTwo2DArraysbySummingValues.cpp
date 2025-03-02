da802711bc59039850765ad5c2c487d498993d92
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-03-02 10:39:26 +0530

    Create file

---
+++ 2570-MergeTwo2DArraysbySummingValues/2570-MergeTwo2DArraysbySummingValues.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        int i = 0,j = 0;
+        int m = nums1.size(), n= nums2.size();
+        vector<vector<int>> ans;
+        while(i < m && j < n){
+            if(nums1[i][0] < nums2[j][0]){
+                ans.push_back(nums1[i]);
+                i++;
+            } else if(nums1[i][0] == nums2[j][0]){
+                ans.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++; j++;
+            } else {
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i < m){
+            ans.push_back(nums1[i]);
+                i++;
+        } 
+        while(j < n){
+            ans.push_back(nums2[j]);
+                j++;
+        }
+        return ans;
+    }
+};
