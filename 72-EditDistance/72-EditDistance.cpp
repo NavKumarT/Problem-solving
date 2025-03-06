b094b69673db301f6f425cb5ead64ec65c236e55
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-03-06 23:23:06 +0530

    Create file

---
+++ 72-EditDistance/72-EditDistance.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        // if(m == 0 || n == 0)
+        //     return m == 0 ? n : m;
+        vector<vector<int>> memo(m+1, vector<int>(n+1, 0));
+        for(int i = 0; i <= m; i++){
+            memo[i][0] = i;
+        }
+        for(int i = 0; i <= n; i++){
+            memo[0][i] = i;
+        }
+        for(int i = 1;i<= m; i++){
+            for(int j = 1; j<= n;j++){
+                if(word1[i-1] == word2[j-1])
+                    memo[i][j] = memo[i-1][j-1];
+                else 
+                    memo[i][j] = min(memo[i-1][j], min(memo[i][j-1], memo[i-1][j-1]))+1;
+            }
+        }
+        return memo[m][n];
+    }
+};
