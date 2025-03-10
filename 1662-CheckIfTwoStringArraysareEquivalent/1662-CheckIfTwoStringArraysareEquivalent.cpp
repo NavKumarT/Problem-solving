66b3fa1e7f26dd6361ebd2025ef57fb9b1fd9f99
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-03-10 22:31:06 +0530

    File updated at 3/10/2025, 10:31:06 PM

--- 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.cpp
+++ 1662-CheckIfTwoStringArraysareEquivalent/1662-CheckIfTwoStringArraysareEquivalent.cpp
@@ -1,11 +1,26 @@
+// Last updated: 3/10/2025, 10:31:06 PM
 class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string a = "", b = "";
-        for(string s : word1)
-            a += s;
-        for(string s : word2)
-            b += s;
-        return a == b;
+        int i = 0, j = 0;
+        int c1 = 0, c2 = 0;
+        while(c1 < word1.size() && c2 < word2.size()){
+            string a = word1[c1];
+            string b = word2[c2];
+
+            if(a[i] != b[j])
+                return false;
+            i++;
+            j++;
+            if(i >= word1[c1].length()){
+                c1++;
+                i = 0;
+            }
+            if(j >= word2[c2].length()){
+                c2++;
+                j = 0;
+            }
+        }
+        return c1 == word1.size() && c2 == word2.size();
     }
 };
