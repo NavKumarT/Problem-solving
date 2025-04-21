d671089aa79f760e21688b69b11f050e3691ba78
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-04-21 15:57:14 +0530

    Create file

---
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.cpp
@@ -0,0 +1,33 @@
+// Last updated: 21/04/2025, 15:57:13
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        int maxLevel = INT_MIN;
+        int level = 0;
+        int ans = 0;
+        traversal(root, level, maxLevel, ans);
+        return ans;
+    }
+
+    void traversal(TreeNode* root, int level, int &maxLevel, int &ans){
+        if(root == nullptr)
+            return;
+        if(level > maxLevel){
+            maxLevel = level;
+            ans = root->val;
+        }
+        traversal(root->left, level+1, maxLevel, ans);
+        traversal(root->right, level+1, maxLevel, ans);
+    }
+};
