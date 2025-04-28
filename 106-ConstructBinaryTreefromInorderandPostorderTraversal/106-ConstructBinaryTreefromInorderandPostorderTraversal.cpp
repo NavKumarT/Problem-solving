f6a17e2725944ea7637a15f52399dc5084d2282f
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-04-28 18:48:32 +0530

    Create file

---
+++ 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.cpp
@@ -0,0 +1,33 @@
+// Last updated: 28/04/2025, 18:48:32
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int i = inorder.size()-1;
+                return buildRecursive( inorder,  postorder,  0,  inorder.size()-1, i);
+    }
+
+    TreeNode* buildRecursive(vector<int> inorder, vector<int> postorder, int left, int right, int &i){
+        if(left > right)
+            return NULL;
+        TreeNode* node = new TreeNode(postorder[i]);
+        int pos = 0;
+        for(int x = 0; x <= postorder.size()-1; x++)
+            if(postorder[i] == inorder[x])
+                pos = x;
+        i--;
+        node->right =  buildRecursive( inorder,  postorder,  pos+1,  right, i);
+        node->left =  buildRecursive( inorder,  postorder,  left,  pos-1, i);
+        return node;
+    }
+};
