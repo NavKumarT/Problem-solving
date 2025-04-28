d54188c0dc687cd9ccf7dc213a5d57ae9ed71e5e
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-04-28 18:50:15 +0530

    File updated at 28/04/2025, 18:50:15

--- 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.cpp
+++ 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.cpp
@@ -1,15 +1,4 @@
-// Last updated: 28/04/2025, 18:48:32
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Last updated: 28/04/2025, 18:50:15
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
