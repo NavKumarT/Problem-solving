5337abc45922ebc4188e77955a07a6c70e9daca6
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-04-28 18:52:21 +0530

    File updated at 28/04/2025, 18:52:22

--- 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.cpp
+++ 106-ConstructBinaryTreefromInorderandPostorderTraversal/106-ConstructBinaryTreefromInorderandPostorderTraversal.cpp
@@ -1,22 +1,26 @@
-// Last updated: 28/04/2025, 18:50:30
+// Last updated: 28/04/2025, 18:52:22
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int i = inorder.size()-1;
-                return buildRecursive( inorder,  postorder,  0,  inorder.size()-1, i);
+        unordered_map<int, int> mp;
+        for(int i = 0; i < inorder.size(); i++)
+            mp[inorder[i]] = i;
+        return buildRecursive( inorder,  postorder,  0,  inorder.size()-1, i, mp);
     }
 
-    TreeNode* buildRecursive(vector<int> &inorder, vector<int> &postorder, int left, int right, int &i){
+    TreeNode* buildRecursive(vector<int> &inorder, vector<int> &postorder, int left, int right, int &i, unordered_map<int, int> &mp){
         if(left > right)
             return NULL;
         TreeNode* node = new TreeNode(postorder[i]);
         int pos = 0;
-        for(int x = 0; x <= postorder.size()-1; x++)
-            if(postorder[i] == inorder[x])
-                pos = x;
+        pos = mp[postorder[i]];
+        // for(int x = 0; x <= postorder.size()-1; x++)
+        //     if(postorder[i] == inorder[x])
+        //         pos = x;
         i--;
-        node->right =  buildRecursive( inorder,  postorder,  pos+1,  right, i);
-        node->left =  buildRecursive( inorder,  postorder,  left,  pos-1, i);
+        node->right =  buildRecursive( inorder,  postorder,  pos+1,  right, i, mp);
+        node->left =  buildRecursive( inorder,  postorder,  left,  pos-1, i, mp);
         return node;
     }
 };
