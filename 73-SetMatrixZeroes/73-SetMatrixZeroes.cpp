3b3a463873b17170933648eceebd81185ee28d96
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-06-03 14:13:17 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.cpp
@@ -0,0 +1,24 @@
+// Last updated: 03/06/2025, 14:13:17
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int> s_rows, s_cols;
+        int rows = matrix.size(), cols = matrix[0].size();
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(matrix[i][j] == 0){
+                    s_rows.insert(i);
+                    s_cols.insert(j);
+                }
+            }
+        }
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(s_rows.find(i) != s_rows.end() || s_cols.find(j) != s_cols.end()){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
