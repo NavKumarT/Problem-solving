a243506f6aa9f79c2d6eb519c5ad0dbfa60c7626
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:50:04 +0530

    Create 817-DesignHashmap

---
+++ 817-DesignHashmap/817-DesignHashmap.cpp
@@ -0,0 +1,32 @@
+// Last updated: 8/29/2025, 9:50:03 PM
+class MyHashMap {
+private: 
+unordered_map<int, int> mp;
+public:
+    MyHashMap() {
+        // pass 
+    }
+    
+    void put(int key, int value) {
+        mp[key] = value;
+    }
+    
+    int get(int key) {
+        if(mp.find(key) == mp.end())
+            return -1;
+        return mp[key];
+    }
+    
+    void remove(int key) {
+        mp.erase(key);
+        return;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
