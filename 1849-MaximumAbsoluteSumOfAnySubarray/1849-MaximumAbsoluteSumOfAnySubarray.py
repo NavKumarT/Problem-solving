3cec500616a53ab079f3cb3e7533e782f7447e44
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:52 +0530

    Create 1849-MaximumAbsoluteSumOfAnySubarray

---
+++ 1849-MaximumAbsoluteSumOfAnySubarray/1849-MaximumAbsoluteSumOfAnySubarray.py
@@ -0,0 +1,17 @@
+# Last updated: 8/29/2025, 9:48:52 PM
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        max_positive_sum = 0
+        max_negative_sum = 0
+
+        curr_positive_sum = 0
+        curr_negative_sum = 0
+
+        for num in nums:
+            curr_positive_sum = max(curr_positive_sum + num, num)
+            max_positive_sum = max(max_positive_sum, curr_positive_sum)
+
+            curr_negative_sum = min(curr_negative_sum + num, num)
+            max_negative_sum = min(max_negative_sum, curr_negative_sum)
+
+        return max(max_positive_sum, abs(max_negative_sum))
