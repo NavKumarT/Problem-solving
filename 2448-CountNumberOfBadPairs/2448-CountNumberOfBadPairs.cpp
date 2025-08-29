eff7e42f59a1ad1b5893c82fc69a86390d7ffa7b
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:20 +0530

    Create 2448-CountNumberOfBadPairs

---
+++ 2448-CountNumberOfBadPairs/2448-CountNumberOfBadPairs.cpp
@@ -0,0 +1,19 @@
+// Last updated: 8/29/2025, 9:48:19 PM
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        unordered_map<long long, long long> mp;
+        long long n =nums.size();
+        for(int i = 0; i < n; i++){
+            mp[i-nums[i]]++;
+        }
+        long long  goodPairs = 0;
+        for(auto it : mp){
+            if(it.second > 1){
+                goodPairs += (it.second * (it.second - 1)) / 2;
+            }
+        }
+        long long totalPairs = (n * (n-1)) / 2;
+        return totalPairs - goodPairs;
+    }
+};
