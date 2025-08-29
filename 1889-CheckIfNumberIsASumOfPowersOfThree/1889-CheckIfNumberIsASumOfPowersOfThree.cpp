67036d745b0d29d452f492075ff481545e2e77f0
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:50 +0530

    Create 1889-CheckIfNumberIsASumOfPowersOfThree

---
+++ 1889-CheckIfNumberIsASumOfPowersOfThree/1889-CheckIfNumberIsASumOfPowersOfThree.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/29/2025, 9:48:50 PM
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        int power = 0;
+
+        // Find the largest power that is smaller or equal to n
+        while (pow(3, power) <= n) {
+            power++;
+        }
+
+        while (n > 0) {
+            // Add current power to the sum
+            if (n >= pow(3, power)) n -= pow(3, power);
+            // We cannot use the same power twice
+            if (n >= pow(3, power)) return false;
+            // Move to the next power
+            power--;
+        }
+
+        // n has reached 0
+        return true;
+    }
+};
