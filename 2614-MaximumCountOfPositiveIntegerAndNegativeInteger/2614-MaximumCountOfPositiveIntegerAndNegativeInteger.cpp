3ffc9dc791acac8d8a1f607e6f6ad5cae9fd9728
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:09 +0530

    Create 2614-MaximumCountOfPositiveIntegerAndNegativeInteger

---
+++ 2614-MaximumCountOfPositiveIntegerAndNegativeInteger/2614-MaximumCountOfPositiveIntegerAndNegativeInteger.cpp
@@ -0,0 +1,74 @@
+// Last updated: 8/29/2025, 9:48:08 PM
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        int pos = -1, neg = -1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] > 0){
+                if(mid == 0 || nums[mid-1] <= 0){
+                    pos = mid;
+                    break;
+                } else {
+                    high = mid-1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        low = 0; high = nums.size()-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] < 0){
+                if(mid == nums.size()-1 || nums[mid+1] >= 0){
+                    neg = mid;
+                    break;
+                } else {
+                    low = mid+1;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+        low = 0; high = nums.size()-1; 
+        int fzero = -1, szero = -1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == 0){
+                if(mid == nums.size()-1 || nums[mid+1] > 0){
+                    szero = mid;
+                    break;
+                } else {
+                    low = mid+1;
+                }
+            } else if(nums[mid] > 0){
+                high = mid - 1;
+            } else {
+                low = mid +1;
+            }
+        }
+        low = 0; high = nums.size()-1; 
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] == 0){
+                if(mid == 0 || nums[mid-1] < 0){
+                    fzero = mid;
+                    break;
+                } else {
+                    high = mid-1;
+                }
+            } else {
+                low = mid + 1;
+            }
+        }
+        int zeroes = (szero == -1 ) ? 0 : szero-fzero+1;
+        if(pos == -1 || neg == -1)
+            return nums.size()-zeroes;
+        
+        else 
+            return neg+1 > nums.size()-pos ? neg+1 : nums.size()-pos;
+        
+    }
+};
