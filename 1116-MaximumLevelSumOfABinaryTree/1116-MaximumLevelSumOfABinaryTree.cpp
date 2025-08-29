dc16643a14813cee5a537d4c956037555152e2b3
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:49:34 +0530

    Create 1116-MaximumLevelSumOfABinaryTree

---
+++ 1116-MaximumLevelSumOfABinaryTree/1116-MaximumLevelSumOfABinaryTree.cpp
@@ -0,0 +1,40 @@
+// Last updated: 8/29/2025, 9:49:33 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        int level = 1;
+        int ans = 1;
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxLevelSum = INT_MIN;
+        while(q.empty() == false){
+            int n = q.size();
+            int levelSum = 0;
+            for(int i = 0; i < n; i++){
+                TreeNode* node = q.front(); q.pop();
+                levelSum += node->val;
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            if(levelSum > maxLevelSum){
+                maxLevelSum = levelSum;
+                ans = level;
+            }
+            level++;
+        }
+        return ans;
+    }
+};
