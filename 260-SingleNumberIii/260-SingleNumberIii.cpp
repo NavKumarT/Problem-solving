d38da71682eb36c5bc8ac7e174bae611ba26689f
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:51:10 +0530

    Create 260-SingleNumberIii

---
+++ 260-SingleNumberIii/260-SingleNumberIii.cpp
@@ -0,0 +1,18 @@
+// Last updated: 8/29/2025, 9:51:10 PM
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long x = 0;
+        for(int i : nums)
+            x ^= i;
+        long long sb = x & (~(x-1));
+        int res1 = 0, res2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if((nums[i] & sb) == 0)
+                res1 = res1 ^ nums[i];
+            else
+                res2 = res2 ^ nums[i];
+        }
+        return {res1, res2};
+    }
+};
