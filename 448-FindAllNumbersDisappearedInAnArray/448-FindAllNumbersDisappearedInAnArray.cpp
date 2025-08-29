5450acbfc95d87831cad42479612abe0cbe5aa3b
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:50:47 +0530

    Create 448-FindAllNumbersDisappearedInAnArray

---
+++ 448-FindAllNumbersDisappearedInAnArray/448-FindAllNumbersDisappearedInAnArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 8/29/2025, 9:50:47 PM
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        for(int i = 0; i < nums.size(); i++){
+            nums[abs(nums[i])-1] = abs(nums[abs(nums[i])-1]) * -1;
+        }        
+        vector<int> v;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] > 0)
+                v.push_back(i+1);
+        }
+        return v;
+    }
+}; 
