ce4584d3f2eb7544b38bd7cab9562dd8d258119a
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:53:12 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,23 @@
+// Last updated: 8/29/2025, 9:53:12 PM
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int j = 0; // current index of the needle to compare
+        int m = haystack.length();  // length of the haystack length 
+        int n = needle.length(); // length of the needle length 
+        int start = -1;
+        for(int i = 0; i < m; ){
+            int start = i;
+            while(haystack[i] == needle[j]){
+                i++;
+                j++;
+                if(j == n){
+                    return i-n;
+                } 
+            }
+            j = 0;
+            i = start + 1;
+        }
+        return -1;
+    }
+};
