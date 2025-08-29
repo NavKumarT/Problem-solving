12dded029e0f2e5209e286bfa60764dd5329e66e
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:49:11 +0530

    Create 1460-NumberOfSubstringsContainingAllThreeCharacters

---
+++ 1460-NumberOfSubstringsContainingAllThreeCharacters/1460-NumberOfSubstringsContainingAllThreeCharacters.cpp
@@ -0,0 +1,26 @@
+// Last updated: 8/29/2025, 9:49:11 PM
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int ans = 0;
+        int start = 0;
+        int end = 0;
+        int n = s.length();
+
+        unordered_map<char, int> ss;
+        ss[s[start]]++;
+        while(end < n && start <= end){
+            if(ss.size() == 3){
+                ans += n-end;
+                ss[s[start]]--;
+                if(ss[s[start]] == 0)
+                    ss.erase(s[start]);
+                start++;
+            } else {
+                end++;
+                ss[s[end]]++;
+            }
+        }
+        return ans;
+    }
+};
