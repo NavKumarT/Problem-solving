ee655db16c311fffebbb6c9826c1fbf75777b231
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:49:55 +0530

    Create 925-ConstructBinaryTreeFromPreorderAndPostorderTraversal

---
+++ 925-ConstructBinaryTreeFromPreorderAndPostorderTraversal/925-ConstructBinaryTreeFromPreorderAndPostorderTraversal.cpp
@@ -0,0 +1,55 @@
+// Last updated: 8/29/2025, 9:49:55 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        unordered_set<int> visited;
+        unordered_map<int, int> postOrderIndex;
+        int n = postorder.size();
+        for(int i = 0; i < n; i++){
+            postOrderIndex[postorder[i]] = i;
+        }
+        return utilsFunc(preorder, postorder, postOrderIndex, n, 0, visited, 0, n-1);
+    }
+
+    TreeNode* utilsFunc(vector<int> &preorder, vector<int> &postorder, unordered_map<int, int> &mp, int n, int index, unordered_set<int> &s, int start, int end){
+        int val = preorder[index];
+        s.insert(val);
+        TreeNode* node = new TreeNode(val);
+        if(start >= end)
+            return node;
+        int leftVal = -1, leftindex = -1;
+        for(int i = index+1;i < n; i++){
+            if(s.find(preorder[i]) == s.end() && mp[preorder[i]] < mp[val]){
+                leftVal = preorder[i];
+                leftindex = i;
+                break;
+            }
+        }
+        if(leftVal == -1 || leftindex == -1)
+            return node;
+        node->left = utilsFunc(preorder, postorder, mp, n, leftindex, s, start, mp[leftVal]);
+        int rightval = -1, rightindex = -1;
+        for(int i = index+1;i < n; i++){
+            if(s.find(preorder[i]) == s.end() && mp[preorder[i]] < mp[val]){
+                rightval = preorder[i];
+                rightindex = i;
+                break;
+            }
+        }
+        if(rightval == -1 || rightindex ==-1)
+            return node;
+        node->right = utilsFunc(preorder, postorder, mp, n, rightindex, s, mp[leftVal]+1, mp[rightval]);
+        return node;
+    }
+};
