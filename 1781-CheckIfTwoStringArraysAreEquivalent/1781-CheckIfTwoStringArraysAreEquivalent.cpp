a4a4a0cd3da2ddae4c7c9f8893546b037425a146
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:55 +0530

    Create 1781-CheckIfTwoStringArraysAreEquivalent

---
+++ 1781-CheckIfTwoStringArraysAreEquivalent/1781-CheckIfTwoStringArraysAreEquivalent.cpp
@@ -0,0 +1,25 @@
+// Last updated: 8/29/2025, 9:48:55 PM
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        int i = 0, j = 0;
+        int c1 = 0, c2 = 0;
+        while(c1 < word1.size() && c2 < word2.size()){
+            string a = word1[c1];
+            string b = word2[c2];
+            if(a[i] != b[j])
+                return false;
+            i++;
+            j++;
+            if(i >= word1[c1].length()){
+                c1++;
+                i = 0;
+            }
+            if(j >= word2[c2].length()){
+                c2++;
+                j = 0;
+            }
+        }
+        return c1 == word1.size() && c2 == word2.size();
+    }
+};
