158464b41d7905aab79fb41a080ab1d3fbeeb0fc
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:53:05 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.cpp
@@ -0,0 +1,14 @@
+// Last updated: 8/29/2025, 9:53:05 PM
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> memo(n, INT_MAX);
+        memo[0] = 0;
+        for(int i = 0; i < n ;i++){
+            for(int j = i; j < n && j <= i+nums[i]; j++)
+                memo[j] = min(memo[j], memo[i]+1);
+        }
+        return memo[n-1];
+    }
+};
