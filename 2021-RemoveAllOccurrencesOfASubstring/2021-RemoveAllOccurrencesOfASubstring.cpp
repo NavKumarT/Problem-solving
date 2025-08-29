fbac52b50c076776b2e6471b79e7c964e28aba5b
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:45 +0530

    Create 2021-RemoveAllOccurrencesOfASubstring

---
+++ 2021-RemoveAllOccurrencesOfASubstring/2021-RemoveAllOccurrencesOfASubstring.cpp
@@ -0,0 +1,36 @@
+// Last updated: 8/29/2025, 9:48:44 PM
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        stack<char> st;
+        int n = s.length();
+        int m = part.length();
+        for(char x : s){
+            st.push(x);
+            if(st.size() >= m && part[m-1] == st.top()){
+                int i = 0;
+                string temp = "";
+                while(i < m){
+                    if(st.top() != part[m-1-i])
+                        break;
+                    temp = st.top() + temp;
+                    st.pop();
+                    i++;
+                }
+                if(temp == part)
+                    continue;
+                else {
+                    for(int i = 0; i < temp.size(); i++){
+                        st.push(temp[i]);
+                    }
+                }
+            }
+        }
+        string ans = "";
+        while(st.empty() == false){
+            ans  = st.top() + ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
