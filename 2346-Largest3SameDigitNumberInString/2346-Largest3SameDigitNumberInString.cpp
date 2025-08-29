dbe8693825133549daf2b401cb277982842984fc
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:24 +0530

    Create 2346-Largest3SameDigitNumberInString

---
+++ 2346-Largest3SameDigitNumberInString/2346-Largest3SameDigitNumberInString.cpp
@@ -0,0 +1,17 @@
+// Last updated: 8/29/2025, 9:48:23 PM
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string largest = "";
+        string temp = num.substr(0, 1);
+        for(int i = 1; i < num.size(); i++){
+            if(num[i] == num[i-1] && temp.length() < 3)
+                temp = temp + num[i];
+            else 
+                temp = num[i];
+            if(temp > largest && temp.length() == 3)
+                largest = temp;
+        }
+        return largest;
+    }
+};
