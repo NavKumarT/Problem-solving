22e26c98c31799554f4af43c4f9900b9c6603ed4
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2025-08-29 21:48:05 +0530

    Create 2677-CousinsInBinaryTreeIi

---
+++ 2677-CousinsInBinaryTreeIi/2677-CousinsInBinaryTreeIi.cpp
@@ -0,0 +1,58 @@
+// Last updated: 8/29/2025, 9:48:05 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        // make 2 level order traversals 
+        // first one to store the sum of the levels 
+        // second one to change the node values 
+        if(root == nullptr)
+            return root;
+        queue<TreeNode*> q;
+        vector<int> levelSum;
+        int level = 0;
+        q.push(root);
+        while(q.empty() == false){
+            int n = q.size();
+            int sum = 0;
+            for(int i = 0; i < n; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                sum += node->val;
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            levelSum.push_back(sum);
+        }
+        level = 0;
+        queue<pair<TreeNode*, int>> qq;
+        qq.push(make_pair(root, root->val));
+        while(qq.empty() == false){
+            int n = qq.size();
+            for(int i = 0; i < n; i++){
+                pair<TreeNode*, int> pp = qq.front();
+                qq.pop();
+                TreeNode* node = pp.first;
+                int siblingSum = pp.second;
+                node->val = levelSum[level] - siblingSum;
+                int sum = 0;
+                if(node->left) sum += node->left->val;
+                if(node->right) sum += node->right->val;
+                if(node->left) qq.push(make_pair(node->left, sum));
+                if(node->right)  qq.push(make_pair(node->right, sum));
+            }
+            level++;
+        }
+        return root;
+    }
+};
