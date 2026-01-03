2fc33f303720d599a1a267815f991f91f2227c2e
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2026-01-03 12:24:56 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 1/3/2026, 12:24:56 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
