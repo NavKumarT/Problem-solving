d9cf194319d5d9621037562dbd6eb1889b1e6f9b
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2026-01-03 12:25:06 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 1/3/2026, 12:25:06 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let count = n;
+    return function() {
+        return count++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
