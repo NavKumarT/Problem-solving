35ad9e92753bb7947717e70c2bec363633863228
YouFoundMe <76838772+NavKumarT@users.noreply.github.com>
2026-06-29 12:35:06 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/version/cpp/39-CombinationSum_20260629_123505.cpp
@@ -0,0 +1,26 @@
+// Last updated: 6/29/2026, 12:35:05 PM
+1class Solution {
+2public:
+3    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+4        vector<vector<int>> ans;
+5        vector<int> temp;
+6        int start = 0;
+7        backtracking(candidates, target, ans, temp, start);
+8        return ans;
+9    }
+10
+11    void backtracking(vector<int> &candidates, int target, vector<vector<int>> &ans, vector<int> &temp, int start){
+12        if(target == 0){
+13            ans.push_back(temp);
+14            return;
+15        }
+16
+17        for(int i = start; i < candidates.size(); i++){
+18            if(candidates[i] > target)
+19                continue;
+20            temp.push_back(candidates[i]);
+21            backtracking(candidates, target-candidates[i], ans, temp, i);
+22            temp.pop_back();
+23        }
+24    }
+25};
